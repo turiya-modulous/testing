69811bba8509fa40d3c0f8c382f3e7f200384db5
turiya-modulous <turiya.pearce-smith@modulous.com>
2022-10-24 15:36:41 +0100

    fix workflow name

--- service/src/verify_page.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-if [ "$(curl -ILs https://turiya-modulous.github.io/testing/ | grep "HTTP/2 200")" ] ; then
-    exit 0
-else
-    echo -e "Failed to get a 200 response from URL" && exit 1;
-fi
